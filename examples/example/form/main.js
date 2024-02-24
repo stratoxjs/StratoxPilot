b8923698ee75d38e4e710542ec5dc0b029ab79ec
Wazabii <daniel@creativearmy.se>
2024-02-24 12:57:18 +0100

    Add examples

---
+++ examples/example/form/main.js
@@ -0,0 +1,42 @@
+import { Router, Dispatcher } from "https://cdn.jsdelivr.net/npm/@stratox/pilot@1.0.3/src/index.js";
+
+const router = new Router();
+const dispatcher = new Dispatcher({
+    catchForms: true
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const content = document.getElementById("content");
+
+    // GET: example.se/
+    router.get('/', function() {
+        content.innerText = "Waiting for form request";
+    });
+
+    router.get('/post/{form:[^/]+}', function(vars, request, path) {
+        content.innerText = "Waiting for form request";
+    });
+
+    router.post('/post/{form:[^/]+}', function(vars, request, path) {
+        const postData = Object.fromEntries(request.post.entries());
+        content.innerText = JSON.stringify(postData);
+    });
+
+    // Will catch 404 and 405 HTTP Status Errors codes
+    // Not required you can also handle it directly in the dispatcher
+    router.get('[STATUS_ERROR]', function(vars, request, path, statusCode) {
+        if(statusCode === 404) {
+            content.innerText = `404 Page not found`;
+        } else {
+            content.innerText = `405 Method not allowed`;
+        }
+    });
+
+    // Dispatch result
+    dispatcher.dispatcher(router, dispatcher.serverParams("fragment"), function(response, statusCode) {
+        // Load router controller
+        response.controller(response.vars, response.request, response.path, statusCode);
+    });
+
+});
---
+++ examples/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * Express is used to run examples
+ */
+const app = express();
+const port = 8000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static('examples'));
+
+app.get('/example/:guideName/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'examples', 'example', req.params.guideName, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at host: localhost, 127.0.0.1 or your local IP number`);
+  console.log(`Server running at port: ${port}`);
+});
